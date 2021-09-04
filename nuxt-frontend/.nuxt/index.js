b77da5ec5489ef7e78633b012ca78652b3e5afc3
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-09-04 19:06:27 +0300

    added nuxt/auth and nuxt/strapi

--- nuxt-frontend/.nuxt/index.js
+++ nuxt-frontend/.nuxt/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-
+import Vuex from 'vuex'
 import Meta from 'vue-meta'
 import ClientOnly from 'vue-client-only'
 import NoSsr from 'vue-no-ssr'
@@ -9,10 +9,16 @@ import NuxtError from './components/nuxt-error.vue'
 import Nuxt from './components/nuxt.js'
 import App from './App.js'
 import { setContext, getLocation, getRouteData, normalizeError } from './utils'
+import { createStore } from './store.js'
 
 /* Plugins */
 
 import nuxt_plugin_plugin_fa0424ae from 'nuxt_plugin_plugin_fa0424ae' // Source: ./components/plugin.js (mode: 'all')
+import nuxt_plugin_cookieuniversalnuxt_8ee86032 from 'nuxt_plugin_cookieuniversalnuxt_8ee86032' // Source: ./cookie-universal-nuxt.js (mode: 'all')
+import nuxt_plugin_httpserver_402bd7ec from 'nuxt_plugin_httpserver_402bd7ec' // Source: ./http.server.js (mode: 'server')
+import nuxt_plugin_http_acf97f9e from 'nuxt_plugin_http_acf97f9e' // Source: ./http.js (mode: 'all')
+import nuxt_plugin_strapi_34e03a50 from 'nuxt_plugin_strapi_34e03a50' // Source: ./strapi.js (mode: 'all')
+import nuxt_plugin_auth_38e8d711 from 'nuxt_plugin_auth_38e8d711' // Source: ./auth.js (mode: 'all')
 
 // Component: <ClientOnly>
 Vue.component(ClientOnly.name, ClientOnly)
@@ -54,8 +60,26 @@ Vue.use(Meta, {"keyName":"head","attribute":"data-n-head","ssrAttribute":"data-n
 
 const defaultTransition = {"name":"page","mode":"out-in","appear":false,"appearClass":"appear","appearActiveClass":"appear-active","appearToClass":"appear-to"}
 
+const originalRegisterModule = Vuex.Store.prototype.registerModule
+
+function registerModule (path, rawModule, options = {}) {
+  const preserveState = process.client && (
+    Array.isArray(path)
+      ? !!path.reduce((namespacedState, path) => namespacedState && namespacedState[path], this.state)
+      : path in this.state
+  )
+  return originalRegisterModule.call(this, path, rawModule, { preserveState, ...options })
+}
+
 async function createApp(ssrContext, config = {}) {
   const router = await createRouter(ssrContext, config)
+
+  const store = createStore(ssrContext)
+  // Add this.$router into store actions/mutations
+  store.$router = router
+
+  // Fix SSR caveat https://github.com/nuxt/nuxt.js/issues/3757#issuecomment-414689141
+  store.registerModule = registerModule
 
   // Create Root instance
 
@@ -64,6 +88,7 @@ async function createApp(ssrContext, config = {}) {
   const app = {
     head: {"title":"nuxt-frontend","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""},{"name":"format-detection","content":"telephone=no"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
 
+    store,
     router,
     nuxt: {
       defaultTransition,
@@ -108,6 +133,9 @@ async function createApp(ssrContext, config = {}) {
     ...App
   }
 
+  // Make app available into store via this.app
+  store.app = app
+
   const next = ssrContext ? ssrContext.next : location => app.router.push(location)
   // Resolve route
   let route
@@ -120,6 +148,7 @@ async function createApp(ssrContext, config = {}) {
 
   // Set context to app.context
   await setContext(app, {
+    store,
     route,
     next,
     error: app.nuxt.error.bind(app),
@@ -146,6 +175,9 @@ async function createApp(ssrContext, config = {}) {
       app.context[key] = value
     }
 
+    // Add into store
+    store[key] = app[key]
+
     // Check if plugin not already installed
     const installKey = '__nuxt_' + key + '_installed__'
     if (Vue[installKey]) {
@@ -167,6 +199,13 @@ async function createApp(ssrContext, config = {}) {
   // Inject runtime config as $config
   inject('config', config)
 
+  if (process.client) {
+    // Replace store state before plugins execution
+    if (window.__NUXT__ && window.__NUXT__.state) {
+      store.replaceState(window.__NUXT__.state)
+    }
+  }
+
   // Add enablePreview(previewData = {}) in context for plugins
   if (process.static && process.client) {
     app.context.enablePreview = function (previewData = {}) {
@@ -178,6 +217,26 @@ async function createApp(ssrContext, config = {}) {
 
   if (typeof nuxt_plugin_plugin_fa0424ae === 'function') {
     await nuxt_plugin_plugin_fa0424ae(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_cookieuniversalnuxt_8ee86032 === 'function') {
+    await nuxt_plugin_cookieuniversalnuxt_8ee86032(app.context, inject)
+  }
+
+  if (process.server && typeof nuxt_plugin_httpserver_402bd7ec === 'function') {
+    await nuxt_plugin_httpserver_402bd7ec(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_http_acf97f9e === 'function') {
+    await nuxt_plugin_http_acf97f9e(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_strapi_34e03a50 === 'function') {
+    await nuxt_plugin_strapi_34e03a50(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_auth_38e8d711 === 'function') {
+    await nuxt_plugin_auth_38e8d711(app.context, inject)
   }
 
   // Lock enablePreview in context
@@ -216,6 +275,7 @@ async function createApp(ssrContext, config = {}) {
   })
 
   return {
+    store,
     app,
     router
   }
--- nuxt-frontend/nuxt.config.js
+++ nuxt-frontend/nuxt.config.js
@@ -1,18 +1,18 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-frontend',
+    title: "nuxt-frontend",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en"
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }
     ]
   },
 
@@ -30,12 +30,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    "@nuxtjs/tailwindcss",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    "@nuxtjs/strapi",
+    "@nuxtjs/auth-next"
   ],
+  strapi: {
+    entities: ["guests", "rooms"]
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
